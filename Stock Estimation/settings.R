da8ed8abec70106ea0ceda415ccbd0d0118d646d
ndysle1 <65475763+ndysle1@users.noreply.github.com>
2021-02-09 10:32:29 -0500

    Reorganizing stock estimation project

---
+++ Stock Estimation/settings.R
@@ -0,0 +1,18 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+#  Stock Estimation Settings & Functions  #
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+## Packages ----
+library(tidyverse)
+library(data.table)
+
+
+## Directories ----
+dir <- list()
+dir$project    <- 'C:/Users/User/Documents/GitHub/R-Projects/Stock Estimation/' 
+dir$notebooks  <- paste0(dir$project,'notebooks/')
+dir$data       <- paste0(dir$project,'data/')
+dir$final_data <- paste0(dir$project,'final_data/')
+
+
+## Functions ----
