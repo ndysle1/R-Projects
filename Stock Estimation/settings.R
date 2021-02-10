d192c0f22df61decd68434558aef2560e19ade9f
ndysle1 <65475763+ndysle1@users.noreply.github.com>
2021-02-09 20:35:30 -0500

    Updating jupyter notebooks 2 and 3

--- Stock Estimation/settings.R
+++ Stock Estimation/settings.R
@@ -16,3 +16,19 @@ dir$final_data <- paste0(dir$project,'final_data/')
 
 
 ## Functions ----
+Name_Changer <- function(x,y,dat){
+  d <- dplyr::select(dat, c(x,y)) %>% setDT()
+  dat <- dplyr::select(dat, -c(x,y)) %>% setDT()
+  d <- d[, x := fifelse(is.na(x), y, x)]
+  d <- d[, x := ifelse(is.na(x), round(x,4), x)]
+  d <- dplyr::select(d, -c(1:2)) %>% setDT()
+  setnames(d, 'x', paste0(x))
+  dat <- cbind(dat, d)
+  return(dat)
+}
+
+
+Perc_Missing <- function(x){
+  x <- sum(is.na(x))/length(x)*100
+  return(x)
+}
